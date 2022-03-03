2dbd31951e69c478b203282ce3f78054e2d3cb5f
abebus <anamaev263@gmail.com>
2022-03-03 23:57:58 +0300

    ;

--- src/code.cpp
+++ src/code.cpp
@@ -9,7 +9,7 @@ namespace assignment {
   int squared_diff(int left, int right) {
 
     // Write your code here ...
-      return pow(left - right)
+      return pow(left - right);
 
     return 0;
   }
