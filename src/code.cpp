3f26ed03e6b45d5d16dd107811af9d7eb67d7d35
abebus <anamaev263@gmail.com>
2022-03-07 17:25:34 +0300

    45/76

--- src/code.cpp
+++ src/code.cpp
@@ -19,7 +19,7 @@ namespace assignment {
 
     // Write your code here ...
     if (mask > 0 and bit_pos > 0) {
-        return bool (mask & int (pow(2, bit_pos)));
+        return (mask & int (pow(2, bit_pos)));
     }
     else {
           return false;
@@ -30,13 +30,13 @@ namespace assignment {
   int max3(int left, int middle, int right) {
 
     // Write your code here ...
-    if (left > middle and left > right) {
+    if (left >= middle and left >= right) {
         return left;
     }
-    if (middle > left and middle > right) {
+    if (middle >= left and middle >= right) {
         return middle;
     }
-    if (right > left and right > middle) {
+    if (right >= left and right >= middle) {
         return right;
     }
 
@@ -46,22 +46,43 @@ namespace assignment {
   void swap_args(int *left, int *right) {
 
     // Write your code here ...
+      int temp = *left;
+      *left = *right;
+      *right = temp;
+      return;
   }
 
   // Task 5
   int arr_sum(int *arr, int length) {
 
     // Write your code here ...
+    if (length < 1 or arr == nullptr){
+        return false;
+    }
+    int sum = 0;
+    for (int i = 0; i < length; i++) {
+        sum = sum + arr[i];
+    }
 
-    return 0;
+    return sum;
   }
 
   // Task 6
   int *find_max_elem(int *arr, int length) {
 
     // Write your code here ...
-
-    return nullptr;
+    if (arr == nullptr or length < 1) {
+        return nullptr;
+    }
+    int max = -2147483648;
+    int *pointer = new int;
+    for (int i = 0; i < length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            pointer = &arr[i];
+        }
+    }
+    return pointer;
   }
 
   // Task 7
